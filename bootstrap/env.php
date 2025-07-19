3c9b1f198efae6bb0cd35c807a35a50e18b559cf
Jorge Peraza <jorge@monkeys.cloud>
2025-07-19 16:36:19 -0600

    fix: global add .env variable

--- bootstrap/env.php
+++ bootstrap/env.php
@@ -1,15 +1,12 @@
 <?php
-
 use Dotenv\Dotenv;
 
-$root = dirname(__DIR__);
+$root = dirname(__DIR__, 4);   // /var/www/html
 
 $env  = getenv('APP_ENV') ?: 'dev';
-$dotenv = Dotenv::createImmutable($root, [
-    '.env', '.env.local',
-    ".env.$env",
-    ".env.$env.local",
-]);
 
-$dotenv->safeLoad();
-$dotenv->required(['DB_HOST', 'DB_DATABASE'])->notEmpty();
+Dotenv::createImmutable($root, [
+    '.env', '.env.local',
+    ".env.$env", ".env.$env.local",
+])->safeLoad()
+    ->required(['DB_HOST', 'DB_DATABASE'])->notEmpty();
