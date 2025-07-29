df55993e9ca6ababaa196d0450771042fb26a205
Amanar Marouane <marouane.amanar07@gmail.com>
2025-07-29 10:13:47 +0100

    refactor(logger): add context enrichment and log level validation

--- src/Logger/FrameworkLoggerInterface.php
+++ src/Logger/FrameworkLoggerInterface.php
@@ -6,20 +6,13 @@ use Psr\Log\LoggerInterface;
 
 interface FrameworkLoggerInterface extends LoggerInterface
 {
-    public function emergency($message, array $context = []): void;
-    public function alert($message, array $context = []): void;
-    public function critical($message, array $context = []): void;
-    public function error($message, array $context = []): void;
-    public function warning($message, array $context = []): void;
-    public function notice($message, array $context = []): void;
-    public function info($message, array $context = []): void;
-    public function debug($message, array $context = []): void;
-    public function log($level, $message, array $context = []): void;
-
     public function smartLog(string $message, array $context = []): void;
 
     public function setLogger(LoggerInterface $logger): self;
+
     public function getLogger(): LoggerInterface;
+
     public function setEnvironment(string $env): self;
+
     public function getEnvironment(): string;
 }
